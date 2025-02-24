cd88e921b6bdbe7fbd9f3e6449864b937cf36329
chandrawijaya <chandrawjy391@gmail.com>
2025-02-24 18:04:47 +0700

    refactor: apply modifiers to optimize function logic

--- beginner_solidity/zombiefeeding.sol
+++ beginner_solidity/zombiefeeding.sol
@@ -19,8 +19,7 @@ modifier ownerOf(uint _zombieId) {
     _zombie.readyTime = uint32(block.timestamp + cooldownTime);
   }
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public ownerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
